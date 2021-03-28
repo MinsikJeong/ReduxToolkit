9bb921ecbbf57d81fa8a15823588e631c9b307d4
Minsik Jeong <minsigi0627@gmail.com>
2021-03-29 02:42:37 +0900

    2. Write Vanilla Code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 1. Redux without React
 Redux without React (vanilla Redux)
+
+# 2. Redux without React
+delete cra code and make vanilla code
--- src/index.js
+++ src/index.js
@@ -8,3 +8,31 @@
 //   </React.StrictMode>,
 //   document.getElementById('root')
 // );
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+let count = 0;
+
+number.innerText = count;
+
+const updateCount = () => {
+  number.innerText = count;
+}
+
+const handleAdd = () => {
+  console.log('add!');
+  // count = count + 1;
+  count ++;
+  updateCount();
+}
+const handleMinus = () => {
+  console.log('minus!');
+  // count = count - 1;
+  count --;
+  updateCount();
+}
+
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus)
